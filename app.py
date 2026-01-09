4b4ff9706fb6c37dd167c21b33aed0dd3432da7e
SHAHAB AHMED KIYANI <107090017+ShahabAhmed56@users.noreply.github.com>
2026-01-09 15:36:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-# test 
+# test 23
 @app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
